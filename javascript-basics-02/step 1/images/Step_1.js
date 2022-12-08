b3ffc4ecebb9870795bee3ef1be4b0865a874eb0
Ayashahl <ayashahal055@gmail.com>
2022-12-08 12:38:51 +0200

    completed step1

---
+++ javascript-basics-02/step 1/images/Step_1.js
@@ -0,0 +1,7 @@
+let image = document.querySelector("#image1")
+//var image= document.getElementById("image");
+
+image.addEventListener("mouseover",function(){
+    console.log("hi")
+    image.src = "images/image1_2.jpg"
+})
